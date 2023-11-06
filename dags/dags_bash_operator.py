b3443743be0fd9df8012157fbc4e800c5efefaee
choeji2022 <choeji2022@gmail.com>
2023-11-06 22:29:48 +0900

    dags_bash_operator 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2023, 11, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+     # [START howto_operator_bash]
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2
