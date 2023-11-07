ea5b2e90b8231a293b475b8c8a5be1277628b6df
choeji2022 <choeji2022@gmail.com>
2023-11-07 20:52:23 +0900

    dags 연결 테스트 수정

---
+++ dags/dags_conn_test.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_conn_test",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2023, 11, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+     # [START howto_operator_bash]
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+    bash_t3 = BashOperator(
+        task_id="bash_t3",
+        bash_command="echo $HOSTNAME",
+    )
+    bash_t4 = BashOperator(
+        task_id="bash_t4",
+        bash_command="echo $HOSTNAME",
+    )
+    bash_t5 = BashOperator(
+        task_id="bash_t5",
+        bash_command="echo $HOSTNAME",
+    )
+    bash_t6 = BashOperator(
+        task_id="bash_t6",
+        bash_command="echo $HOSTNAME",
+    )
+    bash_t7 = BashOperator(
+        task_id="bash_t7",
+        bash_command="echo $HOSTNAME",
+    )
+    bash_t8 = BashOperator(
+        task_id="bash_t8",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> [bash_t2, bash_t3] >> bash_t4 
+    bash_t5 >> bash_t4
+    [bash_t4, bash_t7] >> bash_t6 >> bash_t8
