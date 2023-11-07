84922fa938fdc328fd7eae00ffbf009ed036969e
choeji2022 <choeji2022@gmail.com>
2023-11-08 00:29:15 +0900

    dag_email 업로드 수정

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -15,5 +15,6 @@ with DAG(
         task_id = "send_eamil_task",
         to= 'choeji2022@gmail.com',
         subject="airflow 성공 메일",
-        html_content="airflow 작업이 완료되었습니다"
+        html_content="airflow 작업이 완료되었습니다",
+        conn_id="email_smtp_id"
     )
